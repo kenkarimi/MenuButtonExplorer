f7ee4f71a36099cf6ed73780b62a013a44b71f69
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-11-04 16:54:32 +0300

    Initial Commit

---
+++ MenuButtonExplorer/MenuButtonExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuButtonExplorerApp.swift
+//  MenuButtonExplorer
+//
+//  Created by Kennedy Karimi on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuButtonExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
